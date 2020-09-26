16c074af68b5ee043b749751efacb7f732fda15d
Igor <GI_Something@protonmail.com>
2020-09-26 11:32:24 +0300

    test: Add tests for Main.createInitialGameMessage method

--- src/test/java/bullscows/MainTest.java
+++ src/test/java/bullscows/MainTest.java
@@ -3,9 +3,13 @@ package bullscows;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 import static bullscows.Main.grade;
+import static bullscows.Main.createInitialGameMessage;
 
 public class MainTest {
+    public final String SYMBOLS = "0123456789abcdefghijklmnopqrstuvwxyz";
 
     @Test
     public void testGrade() {
@@ -19,5 +23,14 @@ public class MainTest {
         assertArrayEquals(grade("1234", "1111"), new int[]{3, 1});
         assertArrayEquals(grade("1234", "1212"), new int[]{2, 2});
         assertArrayEquals(grade("9876543210", "9072222123"), new int[]{8, 2});
+    }
+
+    @Test
+    public void testCreateInitialGameMessage() {
+        assertEquals(createInitialGameMessage(10, SYMBOLS, 10), "The secret is prepared: ********** (0-9).");
+        assertEquals(createInitialGameMessage(5, SYMBOLS, 11), "The secret is prepared: ***** (0-9, a-a).");
+        assertEquals(createInitialGameMessage(3, SYMBOLS, 36), "The secret is prepared: *** (0-9, a-z).");
+        assertEquals(createInitialGameMessage(12, SYMBOLS, 12), "The secret is prepared: ************ (0-9, a-b).");
+        assertEquals(createInitialGameMessage(36, SYMBOLS, 36), "The secret is prepared: ************************************ (0-9, a-z).");
     }
 }
